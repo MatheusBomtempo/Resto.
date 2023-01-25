e6cbfa72614c17a90cd32a0d6bb546af33a31da3
Matheus Bomtempo <99459985+MatheusBomtempo@users.noreply.github.com>
2023-01-25 13:39:29 -0300

    pronto

---
+++ script.js
@@ -0,0 +1,88 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  grabCursor: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//////////////////////////////////
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('header nav');
+
+menu.onclick = () =>
+{
+menu.classList.toggle('fa-times');
+navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  section.forEach(sec =>{
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links =>{
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+      });
+    };
+
+  });
+
+}
+
+
+///////////////////////////////////////////////
+
+var swiper = new Swiper(".swiper-review", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    loop:true,
+    breakpoints: {
+      0: {
+          slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  
+
+////////////////////////////////////////////////
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.scrollspy');
+  var instances = M.ScrollSpy.init(elems, options);
+});
+/////////////////////////////////////////////////////
